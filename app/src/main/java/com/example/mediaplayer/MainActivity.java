082c2ca49ab0ab3bc1c99bbf0b5dcf86c75667b9
Daniel Penelva <d4n.andrade@gmail.com>
2023-05-09 03:09:34 -0300

    Controlando o volume (seekBar)

--- app/src/main/java/com/example/mediaplayer/MainActivity.java
+++ app/src/main/java/com/example/mediaplayer/MainActivity.java
@@ -2,13 +2,18 @@ package com.example.mediaplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
 
-    MediaPlayer mediaPlayer;
+    private MediaPlayer mediaPlayer;
+    private SeekBar seekBarVolume;
+    private AudioManager audioManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +21,48 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
        mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.ceuazul);
+
+       inicializarSeekBar();
+    }
+
+    private void inicializarSeekBar(){
+        seekBarVolume = findViewById(R.id.seekVolume);
+
+        /* Configura audio manager - consegue saber o volume atual que está executando para o usuário e
+        qual é o volume máximo que consegue aumentar essa música no celular dele. */
+
+        audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);  //recupera alguns serviços do sistema
+
+        int volumeMaximo = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC); //recupera o valor do volume máximo
+
+        int volumeAtual = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC); // recupera o valor do volume atual
+
+        /* Configura os valores máximos para o SeekBar */
+        seekBarVolume.setMax(volumeMaximo);
+
+        /* Configura o progresso atual do SeekBar */
+        seekBarVolume.setProgress(volumeAtual);
+
+        /* configura o incremento do volume (aumentando ou diminuindo) quando arrasta o progresso do SeekBar */
+        seekBarVolume.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            // Vamos usar apenas esse método que correspondo qnd o usuário manipula a seekBar
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, progress, 0);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
     }
 
     // Método para executar o play
