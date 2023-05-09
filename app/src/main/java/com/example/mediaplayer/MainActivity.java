0c507cf8ab53533bb0132063bf91a78e5879d982
Daniel Penelva <d4n.andrade@gmail.com>
2023-05-09 02:29:33 -0300

    implementando o método executarSom (play)

--- app/src/main/java/com/example/mediaplayer/MainActivity.java
+++ app/src/main/java/com/example/mediaplayer/MainActivity.java
@@ -2,14 +2,27 @@ package com.example.mediaplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    MediaPlayer mediaPlayer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+       mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.ceuazul);
+    }
+
+    // Método para executar o play
+    public void executarSom(View view){
+
+        if(mediaPlayer != null){
+            mediaPlayer.start();
+        }
     }
 }
